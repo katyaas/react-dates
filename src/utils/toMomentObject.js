c42f4e9b15eac61937f87292fd8b8aee46845bd5
Ekaterina Strekalova <koshkina.ekaterina@gmail.com>
2020-03-11 16:16:44 +0300

    remove time in toMomentObject

--- src/utils/toMomentObject.js
+++ src/utils/toMomentObject.js
@@ -4,9 +4,9 @@ import { DISPLAY_FORMAT, ISO_FORMAT } from '../constants';
 
 export default function toMomentObject(dateString, customFormat) {
   const dateFormats = customFormat
-    ? [customFormat, DISPLAY_FORMAT, ISO_FORMAT]
+    ? customFormat
     : [DISPLAY_FORMAT, ISO_FORMAT];
 
-  const date = moment(dateString, dateFormats, true);
-  return date.isValid() ? date.hour(12) : null;
+  const date = moment.utc(dateString, dateFormats, true);
+  return date.isValid() ? date : null;
 }
